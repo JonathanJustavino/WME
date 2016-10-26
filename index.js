52c0af5a018d2368ac9d305f02180ebc9420c6ee
JonathanJustavino <s3190208@mail.zih.tu-dresden.de>
2016-10-26 19:20:30 +0200

    Add json version of the table data and implement functions to display them in the html file

--- index.js
+++ index.js
@@ -1,25 +1,474 @@
-//var csv is the CSV file with headers
-function csvJSON(csv){
+var myData = [
+ {
+   "FIELD1": "id ",
+   "FIELD2": "name                ",
+   "FIELD3": "birth rate per 1000",
+   "FIELD4": "cell phones per 100",
+   "FIELD5": "children per woman",
+   "FIELD6": "electricity consumption per capita",
+   "FIELD7": "gdp_per_capita",
+   "FIELD8": "gdp_per_capita_growth",
+   "FIELD9": "inflation annual",
+   "FIELD10": "internet user per 100",
+   "FIELD11": "life expectancy",
+   "FIELD12": "military expenditure percent of gdp",
+   "FIELD13": "gps_lat      ",
+   "FIELD14": "gps_long"
+ },
+ {
+   "FIELD1": "001",
+   "FIELD2": "Brazil              ",
+   "FIELD3": "16.405             ",
+   "FIELD4": "90.01936334        ",
+   "FIELD5": "1.862             ",
+   "FIELD6": "2201.808724                       ",
+   "FIELD7": "4424.758692   ",
+   "FIELD8": "-1.520402823         ",
+   "FIELD9": "8.228535058     ",
+   "FIELD10": "39.22                ",
+   "FIELD11": "74             ",
+   "FIELD12": "1.615173655                        ",
+   "FIELD13": "-14.235004000",
+   "FIELD14": "-51.925280000"
+ },
+ {
+   "FIELD1": "002",
+   "FIELD2": "Canada              ",
+   "FIELD3": "10.625             ",
+   "FIELD4": "70.70997244        ",
+   "FIELD5": "1.668             ",
+   "FIELD6": "15119.76414                       ",
+   "FIELD7": "25069.86915   ",
+   "FIELD8": "-3.953353186         ",
+   "FIELD9": "2.944408564     ",
+   "FIELD10": "80.17086651          ",
+   "FIELD11": "80.9           ",
+   "FIELD12": "1.415710422                        ",
+   "FIELD13": "56.130366000 ",
+   "FIELD14": "-106.346771000"
+ },
+ {
+   "FIELD1": "003",
+   "FIELD2": "Chile               ",
+   "FIELD3": "15.04              ",
+   "FIELD4": "97.01862561        ",
+   "FIELD5": "1.873             ",
+   "FIELD6": "3276.06449                        ",
+   "FIELD7": "6451.631126   ",
+   "FIELD8": "-2.610485847         ",
+   "FIELD9": "7.47050527      ",
+   "FIELD10": "38.8                 ",
+   "FIELD11": "78.8           ",
+   "FIELD12": "3.064076139                        ",
+   "FIELD13": "-35.675147000",
+   "FIELD14": "71.542969000"
+ },
+ {
+   "FIELD1": "004",
+   "FIELD2": "China               ",
+   "FIELD3": "13.536             ",
+   "FIELD4": "55.97490921        ",
+   "FIELD5": "1.642             ",
+   "FIELD6": "2632.724637                       ",
+   "FIELD7": "2208.403948   ",
+   "FIELD8": "8.648414427          ",
+   "FIELD9": "6.684109668     ",
+   "FIELD10": "28.97659939          ",
+   "FIELD11": "75.6           ",
+   "FIELD12": "2.24110794                         ",
+   "FIELD13": "35.86166000  ",
+   "FIELD14": "104.19539700"
+ },
+ {
+   "FIELD1": "005",
+   "FIELD2": "Colombia            ",
+   "FIELD3": "20.605             ",
+   "FIELD4": "92.34584564        ",
+   "FIELD5": "2.405             ",
+   "FIELD6": "1041.994137                       ",
+   "FIELD7": "3137.695335   ",
+   "FIELD8": "0.2081538559         ",
+   "FIELD9": "3.666941163     ",
+   "FIELD10": "30                   ",
+   "FIELD11": "75.3           ",
+   "FIELD12": "3.833780372                        ",
+   "FIELD13": "4.570868000  ",
+   "FIELD14": "-74.297333000"
+ },
+ {
+   "FIELD1": "006",
+   "FIELD2": "Ecuador             ",
+   "FIELD3": "20.989             ",
+   "FIELD4": "92.84925653        ",
+   "FIELD5": "2.69              ",
+   "FIELD6": "1078.038961                       ",
+   "FIELD7": "1692.067197   ",
+   "FIELD8": "-1.079538461         ",
+   "FIELD9": "7.595866872     ",
+   "FIELD10": "24.6                 ",
+   "FIELD11": "74.1           ",
+   "FIELD12": "3.746501879                        ",
+   "FIELD13": "-1.831239000 ",
+   "FIELD14": "-78.183406000"
+ },
+ {
+   "FIELD1": "007",
+   "FIELD2": "Egypt               ",
+   "FIELD3": "24.83              ",
+   "FIELD4": "69.43661504        ",
+   "FIELD5": "2.919             ",
+   "FIELD6": "1607.918763                       ",
+   "FIELD7": "1911.964501   ",
+   "FIELD8": "2.856917581          ",
+   "FIELD9": "10.10750976     ",
+   "FIELD10": "24.28                ",
+   "FIELD11": "70.2           ",
+   "FIELD12": "2.137305699                        ",
+   "FIELD13": "26.820553000 ",
+   "FIELD14": "30.802498000"
+ },
+ {
+   "FIELD1": "008",
+   "FIELD2": "Finland             ",
+   "FIELD3": "11.127             ",
+   "FIELD4": "144.1530224        ",
+   "FIELD5": "1.86              ",
+   "FIELD6": "15241.61194                       ",
+   "FIELD7": "26205.68832   ",
+   "FIELD8": "-8.791558776         ",
+   "FIELD9": "0.4277308694    ",
+   "FIELD10": "82.53133098          ",
+   "FIELD11": "79.7           ",
+   "FIELD12": "1.501872268                        ",
+   "FIELD13": "61.924110000 ",
+   "FIELD14": "25.748151000"
+ },
+ {
+   "FIELD1": "009",
+   "FIELD2": "France              ",
+   "FIELD3": "12.21              ",
+   "FIELD4": "95.44434226        ",
+   "FIELD5": "1.978             ",
+   "FIELD6": "7339.946832                       ",
+   "FIELD7": "22508.76261   ",
+   "FIELD8": "-3.264056248         ",
+   "FIELD9": "1.050366124     ",
+   "FIELD10": "69.0633593           ",
+   "FIELD11": "81             ",
+   "FIELD12": "2.55313249                         ",
+   "FIELD13": "46.22763800  ",
+   "FIELD14": "2.21374900"
+ },
+ {
+   "FIELD1": "010",
+   "FIELD2": "Germany             ",
+   "FIELD3": "8.136              ",
+   "FIELD4": "127.4188883        ",
+   "FIELD5": "1.376             ",
+   "FIELD6": "6753.05764                        ",
+   "FIELD7": "24368.19561   ",
+   "FIELD8": "-4.886323581         ",
+   "FIELD9": "0.5959234322    ",
+   "FIELD10": "79.48523153          ",
+   "FIELD11": "80             ",
+   "FIELD12": "1.438871341                        ",
+   "FIELD13": "51.165691000 ",
+   "FIELD14": "10.451526000"
+ },
+ {
+   "FIELD1": "011",
+   "FIELD2": "Iceland             ",
+   "FIELD3": "14.738             ",
+   "FIELD4": "107.6604456        ",
+   "FIELD5": "2.123             ",
+   "FIELD6": "51259.18763                       ",
+   "FIELD7": "35310.97441   ",
+   "FIELD8": "-6.990418561         ",
+   "FIELD9": "6.900703626     ",
+   "FIELD10": "92.13686385          ",
+   "FIELD11": "82.2           ",
+   "FIELD12": "0.0820538039                       ",
+   "FIELD13": "64.9630510000",
+   "FIELD14": "-19.0208350000"
+ },
+ {
+   "FIELD1": "012",
+   "FIELD2": "Iraq                ",
+   "FIELD3": "31.585             ",
+   "FIELD4": "65.47478839        ",
+   "FIELD5": "4.276             ",
+   "FIELD6": "1086.323768                       ",
+   "FIELD7": "752.1833548   ",
+   "FIELD8": "1.141874289          ",
+   "FIELD9": "25.22442136     ",
+   "FIELD10": "1.047516616          ",
+   "FIELD11": "68.1           ",
+   "FIELD12": "4.621383386                        ",
+   "FIELD13": "33.223191000 ",
+   "FIELD14": "43.679291000"
+ },
+ {
+   "FIELD1": "013",
+   "FIELD2": "Japan               ",
+   "FIELD3": "8.201              ",
+   "FIELD4": "91.8955442         ",
+   "FIELD5": "1.359             ",
+   "FIELD6": "7838.005685                       ",
+   "FIELD7": "38242.02429   ",
+   "FIELD8": "-6.180260885         ",
+   "FIELD9": "-2.08543109     ",
+   "FIELD10": "77.38468963          ",
+   "FIELD11": "82.8           ",
+   "FIELD12": "1.019445017                        ",
+   "FIELD13": "36.204824000 ",
+   "FIELD14": "138.252924000"
+ },
+ {
+   "FIELD1": "014",
+   "FIELD2": "Kazakhstan          ",
+   "FIELD3": "19.775             ",
+   "FIELD4": "107.7147692        ",
+   "FIELD5": "2.537             ",
+   "FIELD6": "4447.142293                       ",
+   "FIELD7": "2345.86415    ",
+   "FIELD8": "-1.437812068         ",
+   "FIELD9": "19.5422854      ",
+   "FIELD10": "17.91457965          ",
+   "FIELD11": "66.6           ",
+   "FIELD12": "1.105385125                        ",
+   "FIELD13": "48.019573000 ",
+   "FIELD14": "66.923684000"
+ },
+ {
+   "FIELD1": "015",
+   "FIELD2": "Mexico              ",
+   "FIELD3": "19.091             ",
+   "FIELD4": "74.25785259        ",
+   "FIELD5": "2.313             ",
+   "FIELD6": "1869.82352                        ",
+   "FIELD7": "5875.619997   ",
+   "FIELD8": "-7.417438847         ",
+   "FIELD9": "4.033645258     ",
+   "FIELD10": "26.34                ",
+   "FIELD11": "75.5           ",
+   "FIELD12": "0.5396656609                       ",
+   "FIELD13": "23.6345010000",
+   "FIELD14": "-102.5527840000"
+ },
+ {
+   "FIELD1": "016",
+   "FIELD2": "New Zealand         ",
+   "FIELD3": "13.831             ",
+   "FIELD4": "108.7301521        ",
+   "FIELD5": "2.125             ",
+   "FIELD6": "9375.550304                       ",
+   "FIELD7": "14778.16393   ",
+   "FIELD8": "-1.552308788         ",
+   "FIELD9": "3.486782259     ",
+   "FIELD10": "79.82609287          ",
+   "FIELD11": "80.3           ",
+   "FIELD12": "1.140562366                        ",
+   "FIELD13": "-40.900557000",
+   "FIELD14": "174.885971000"
+ },
+ {
+   "FIELD1": "017",
+   "FIELD2": "Nigeria             ",
+   "FIELD3": "40.134             ",
+   "FIELD4": "48.23561006        ",
+   "FIELD5": "6.021             ",
+   "FIELD6": "119.8151486                       ",
+   "FIELD7": "513.5003377   ",
+   "FIELD8": "4.3526073            ",
+   "FIELD9": "9.313146383     ",
+   "FIELD10": "20                   ",
+   "FIELD11": "58.5           ",
+   "FIELD12": "0.8924302789                       ",
+   "FIELD13": "9.0819990000 ",
+   "FIELD14": "8.6752770000"
+ },
+ {
+   "FIELD1": "018",
+   "FIELD2": "Peru                ",
+   "FIELD3": "21.342             ",
+   "FIELD4": "85.86901405        ",
+   "FIELD5": "2.545             ",
+   "FIELD6": "1043.052601                       ",
+   "FIELD7": "2955.186222   ",
+   "FIELD8": "-0.2228977721        ",
+   "FIELD9": "4.49134447      ",
+   "FIELD10": "31.4                 ",
+   "FIELD11": "76.7           ",
+   "FIELD12": "1.348875763                        ",
+   "FIELD13": "-9.189967000 ",
+   "FIELD14": "-75.015152000"
+ },
+ {
+   "FIELD1": "019",
+   "FIELD2": "Russia              ",
+   "FIELD3": "10.828             ",
+   "FIELD4": "161.1162887        ",
+   "FIELD5": "1.537             ",
+   "FIELD6": "6132.978648                       ",
+   "FIELD7": "2806.41483    ",
+   "FIELD8": "-7.749103694         ",
+   "FIELD9": "11.60398093     ",
+   "FIELD10": "29.23584146          ",
+   "FIELD11": "68.3           ",
+   "FIELD12": "4.36259042                         ",
+   "FIELD13": "61.52401000  ",
+   "FIELD14": "105.31875600"
+ },
+ {
+   "FIELD1": "020",
+   "FIELD2": "Saudi Arabia        ",
+   "FIELD3": "23.569             ",
+   "FIELD4": "167.3474553        ",
+   "FIELD5": "2.898             ",
+   "FIELD6": "7430.743897                       ",
+   "FIELD7": "9294.355996   ",
+   "FIELD8": "-2.242127204         ",
+   "FIELD9": "14.36222827     ",
+   "FIELD10": "38                   ",
+   "FIELD11": "77.6           ",
+   "FIELD12": "10.95653405                        ",
+   "FIELD13": "23.88594200  ",
+   "FIELD14": "45.07916200"
+ },
+ {
+   "FIELD1": "021",
+   "FIELD2": "South Africa        ",
+   "FIELD3": "22.113             ",
+   "FIELD4": "93.33587369        ",
+   "FIELD5": "2.5               ",
+   "FIELD6": "4532.021902                       ",
+   "FIELD7": "3697.67368    ",
+   "FIELD8": "-2.732989408         ",
+   "FIELD9": "7.861608575     ",
+   "FIELD10": "10.08745979          ",
+   "FIELD11": "55.8           ",
+   "FIELD12": "1.394530379                        ",
+   "FIELD13": "-30.559482000",
+   "FIELD14": "22.937506000"
+ },
+ {
+   "FIELD1": "022",
+   "FIELD2": "Sweden              ",
+   "FIELD3": "11.72              ",
+   "FIELD4": "112.1241184        ",
+   "FIELD5": "1.937             ",
+   "FIELD6": "14143.01101                       ",
+   "FIELD7": "30885.45914   ",
+   "FIELD8": "-5.976535294         ",
+   "FIELD9": "1.022227082     ",
+   "FIELD10": "91.12326108          ",
+   "FIELD11": "81.2           ",
+   "FIELD12": "1.247701873                        ",
+   "FIELD13": "60.128161000 ",
+   "FIELD14": "18.643501000"
+ },
+ {
+   "FIELD1": "023",
+   "FIELD2": "United Arab Emirates",
+   "FIELD3": "14.027             ",
+   "FIELD4": "153.7997194        ",
+   "FIELD5": "1.903             ",
+   "FIELD6": "9998.291079                       ",
+   "FIELD7": "22507.00157   ",
+   "FIELD8": "-11.99171952         ",
+   "FIELD9": "8.549032358     ",
+   "FIELD10": "64                   ",
+   "FIELD11": "76.1           ",
+   "FIELD12": "5.834881976                        ",
+   "FIELD13": "23.424076000 ",
+   "FIELD14": "53.847818000"
+ },
+ {
+   "FIELD1": "024",
+   "FIELD2": "United Kingdom      ",
+   "FIELD3": "12.195             ",
+   "FIELD4": "130.1742603        ",
+   "FIELD5": "1.89              ",
+   "FIELD6": "5685.635995                       ",
+   "FIELD7": "27933.77767   ",
+   "FIELD8": "-5.019251823         ",
+   "FIELD9": "2.861406642     ",
+   "FIELD10": "77.79971962          ",
+   "FIELD11": "79.7           ",
+   "FIELD12": "2.667209048                        ",
+   "FIELD13": "52.355517700 ",
+   "FIELD14": "-1.174319700"
+ },
+ {
+   "FIELD1": "025",
+   "FIELD2": "United States       ",
+   "FIELD3": "14.191             ",
+   "FIELD4": "89.14911634        ",
+   "FIELD5": "2.002             ",
+   "FIELD6": "12913.71143                       ",
+   "FIELD7": "36539.22823   ",
+   "FIELD8": "-4.342271218         ",
+   "FIELD9": "1.152326348     ",
+   "FIELD10": "71.21181627          ",
+   "FIELD11": "78.3           ",
+   "FIELD12": "4.822730027                        ",
+   "FIELD13": "37.090240000 ",
+   "FIELD14": "-95.712891000"
+ }
+]
 
-  var lines=csv.split("\n");
 
-  var result = [];
-
-  var headers=lines[0].split(",");
-
-  for(var i=1;i<lines.length;i++){
-
-	  var obj = {};
-	  var currentline=lines[i].split(",");
-
-	  for(var j=0;j<headers.length;j++){
-		  obj[headers[j]] = currentline[j];
-	  }
-
-	  result.push(obj);
-
+function jsonToArray(arr, jsonData){
+  for(var i in jsonData){
+    arr.push([i, jsonData [i]]);
   }
-
-  //return result; //JavaScript object
-  return JSON.stringify(result); //JSON
+  return arr
 }
+
+var table = jsonToArray(myData)
+
+// alert(jsonToArray(result, myData))
+
+var tableStart = document.getElementById('my-table')
+
+function propertyCount(obj){
+  return Object.keys(obj).length
+}
+
+function fillTable(obj, append){
+  var i = 1;
+  for (var prop in obj) {
+    if (obj.hasOwnProperty(prop)) {
+      var td = document.createElement('TD');
+      var text = document.createTextNode(prop)
+      console.log('created data' + i++)
+      td.appendChild(text)
+      append.appendChild(td)
+    }
+  }
+}
+
+
+function createTable(tableData){
+  for(var i = 0; i < tableData.length; i++){
+    var tr = document.createElement('TR');
+    fillTable(tableData, tr)
+    tableStart.appendChild(tr)
+  }
+}
+
+createTable(table)
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
